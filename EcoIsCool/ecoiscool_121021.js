ada1d8666f53bf09888f09cb3c3a60a466d8f5c5
edebrand <91876071+edebrand@users.noreply.github.com>
2022-02-02 09:33:14 +0100

    Add files via upload

---
+++ EcoIsCool/ecoiscool_121021.js
@@ -0,0 +1,6 @@
+function mouseOver() {
+    document.getElementById("logo").innerHTML = '<img src="global-warming_hover.png" />';
+}
+function mouseOut() {
+    document.getElementById("logo").innerHTML = '<img src="logo_global_warming.png" />';
+}
